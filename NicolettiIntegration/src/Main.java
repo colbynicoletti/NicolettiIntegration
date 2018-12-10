b584eb5d8ded10ac724c68424228e8176edd64dd
cjn53 <cjn53@DESKTOP-ROT3VD2>
2018-12-09 22:04:40 -0500

    Exception Handling Started

--- NicolettiIntegration/src/Main.java
+++ NicolettiIntegration/src/Main.java
@@ -11,80 +11,81 @@ public class Main {
     Scanner in = new Scanner(System.in);
     boolean badInput = true;
   /* 
+   	//Handled
     Introduction.main();
-    
+    //Handled
     DataTypes.main(in, badInput);
-    
+    // */
     Boolean.main(in, badInput);
-    
+    //
     Integer.main(in, badInput);
-
+	//
     DoubleMethod.main(in, badInput);
-    
+    //
     StringMethod.main(in, badInput);
-    
+    //
     VarAndScope.main(in, badInput);
-
+	//
     FinalMethod.main(in, badInput);
-    
+    //
     StringUses.main(in, badInput);
-    
+    //
     EscapeSequences.main(in, badInput);
-
+	//
     CastingExample.main(in, badInput);
-
+	//
     MethodExample.main(in, badInput);
-    
+    //
     RandomExample.main(in, badInput);
-    
+    //
     IfStatementExample.main(in, badInput);
-    
+    //
     TernaryConstructExample.main(in, badInput);
-    
+    //
     SwitchExample.main(in, badInput);
-
+	//
     CompareExample.main(in, badInput);
-
+	//
     Calculator.main(in, badInput);
-    
+    //
     DoublePlusAndDoubleMinus.main(in, badInput);
-    
+    //
     RelationalOperatorsExample.main(in, badInput);
-    
+    //
     PlusEqualsExample.main(in, badInput);
-    
+    //
     ConditionalOperatorExample.main(in, badInput);
-
+	//
     OperatorPrecedenceExample.main(in, badInput);
-
+	//
     Conclusion.main(badInput);
-    
+    //
     WhileExample.main(in, badInput);
-    
+    //
     ForExample.main(in, badInput);
-    
+    //
     DoWhileLoopExample.main(in, badInput);
-    
+    //
     BreakAndContinue.main(in, badInput);
-    
+    //
     SetterAndGetter.main(in, badInput);
-    
+    //
     OverloadingMethods.main(in, badInput);
-    
+    //
     InheritanceDefinition.main(in, badInput);
-    
+    //
     PolymorphismExample.main(in, badInput);
- 
+ 	//
     SuperAndThis.main(in, badInput);
-     
+    //
     ArrayExamples.main(in, badInput);
-    
+    //
     EnhancedForLoop.main(in, badInput);
-    
+    //
     TwoDimensionalArray.main(in, badInput);
-     
+    //
     ArrayListExample.main(in, badInput);
-    */
+    // 
     SearchTwoDimensionalArray.main(in, badInput);
     
     in.close();
