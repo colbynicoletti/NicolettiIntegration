557e3e1c776fbbd2a327cfeccec3681b5aa525b1
cjn53 <cjn53@DESKTOP-ROT3VD2>
2018-12-12 18:42:35 -0500

    Exception Handling, Continued

--- NicolettiIntegration/src/JavaSubstring.java
+++ NicolettiIntegration/src/JavaSubstring.java
@@ -7,10 +7,25 @@ public class JavaSubstring {
 			Scanner in = new Scanner(System.in);
 	        System.out.println("Enter a one word string:");
 	        String S = in.next();
+	        int start = 0;
+	        int end;
 	        System.out.println("Enter a value that symbolizes the start of your substring:");
-	        int start = in.nextInt();
+	        System.out.println("Hint: It should be less than the length of the string.");
+	        boolean input = true;
+	        while(input){
+	        try {
+	        start = in.nextInt();
+	        }catch(Exception e) {
+	        	System.out.println("Wrong input, enter an integer.");
+	        }
+	        if (start > S.length()) {
+	        	System.out.println("Number is too high, enter another integer.");
+	        }else if(start <= S.length()){
+	        	break;
+	        }
+	        }
 	        System.out.println("Enter a value that symbolizes the end of your substring:");
-	        int end = in.nextInt();
+	        end = in.nextInt();
 	        System.out.println(S.substring(start,end));
 	        System.out.println("Congratulations! You made a substring!");
 		}
