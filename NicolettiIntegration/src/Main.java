b013c289cea3bda51929a69476a3eb6944940e92
cjn53 <cjn53@172.26.205.29>
2018-12-13 01:24:38 -0500

    Handling Done.

--- NicolettiIntegration/src/Main.java
+++ NicolettiIntegration/src/Main.java
@@ -10,80 +10,79 @@ public class Main {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     boolean badInput = true;
-  /* 
-   	//Handled
+ 
     Introduction.main();
-    //Handled
+    
     DataTypes.main(in, badInput);
-    //Handled 
+     
     Boolean.main(in, badInput);
-    //Handled
+    
     Integer.main(in, badInput);
-	//Handled
+	
     DoubleMethod.main(in, badInput);
-    // 
+     
     StringMethod.main(in, badInput);
-    //Handled 
+     
     VarAndScope.main(in, badInput);
-	//Handled 
+	 
     FinalMethod.main(in, badInput);
-    //Handled
+    
     StringUses.main(in, badInput);
-    // 
+     
     EscapeSequences.main(in, badInput);
-	//Handled 
+	 
     CastingExample.main(in, badInput);
-	//Handled 
+	 
     MethodExample.main(in, badInput);
-    //Handled 
+     
     RandomExample.main(in, badInput);
-    //Handled 
+     
     IfStatementExample.main(in, badInput);
-    //Handled 
+     
     TernaryConstructExample.main(in, badInput);
-    //Handled 
+     
     SwitchExample.main(in, badInput);
-	//Handled
+	
     CompareExample.main(in, badInput);
-	//Handled 
+	 
     Calculator.main(in, badInput);
-    //Handled
+    
     DoublePlusAndDoubleMinus.main(in, badInput);
-    //Handled 
+     
     RelationalOperatorsExample.main(in, badInput);
-    //Handled 
+     
     PlusEqualsExample.main(in, badInput);
-    //Handled 
+     
     ConditionalOperatorExample.main(in, badInput);
-	//Handled 
+     
     OperatorPrecedenceExample.main(in, badInput);
-	//Handled 
+	 
     WhileExample.main(in, badInput);
-    //Handled 
+     
     ForExample.main(in, badInput);
-    //Handled 
+     
     DoWhileLoopExample.main(in, badInput);
-    //Handled 
+     
     BreakAndContinue.main(in, badInput);
-    // 
+    
     SetterAndGetter.main(in, badInput);
-    //Handled  
+      
     OverloadingMethods.main(in, badInput);
-    //Handled 
+     
     InheritanceDefinition.main(in, badInput);
-    //Handled 
+     
     PolymorphismExample.main(in, badInput);
- 	//Handled 
+ 	 
     SuperAndThis.main(in, badInput);
-    // */
+     
     ArrayExamples.main(in, badInput);
-    //
+     
     EnhancedForLoop.main(in, badInput);
-    //
+     
     TwoDimensionalArray.main(in, badInput);
-    //
+    
     ArrayListExample.main(in, badInput);
-    // 
+    
     SearchTwoDimensionalArray.main(in, badInput);
     
     in.close();
